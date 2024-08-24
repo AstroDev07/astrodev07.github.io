e8bc2449f138c370b626af3b79db91cf6a411b0a
Diego Enriquez <astrodev07@proton.me>
2024-08-24 23:12:15 +0200

    Fixed the blog/main.js file

--- blog/main.js
+++ blog/main.js
@@ -1,21 +1,6 @@
 let footer = document.getElementById('footer-text')
 footer.innerHTML = 'Diego Enríquez<br/> Copyright© ' + new Date().getFullYear()
-let events = (id, url) => {
-  let obj = document.getElementById(id)
-  obj.addEventListener('click', () => {
-    window.open(url)
-  })
-}
-events('degcodechannel', 'https://t.me/degcode')
-events('twitterlink', 'https://www.x.com/astrodev0703')
-events('personaltg', 'https://t.me/astrofficial')
-events('mygithub', 'https://github.com/astrodev07')
-events('ytchannel', 'https://youtube.com/@astrodev07')
-events('emailink', 'mailto:diegoenriquezpuig07@gmail.com')
-events(
-  'instagram',
-  'https://www.instagram.com/diego.alejandroep?igsh=NWE5dXhjYXN0OTJu'
-)
+
 let changeMode = document.getElementById('change-mode')
 changeMode.addEventListener(
   'click',
